cbac81bad542f8270f05478f2e5c8309fe0a4744
ywt <ywt>
2019-04-15 09:54:44 +0800

    忽略bug修改

---
+++ TestProject/Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    //网站入口控制器
+    public class HomeController : Controller
+    {
+        //首页
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //测试页
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
