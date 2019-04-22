a93d868ce77b96f4d9cab3313a23e990e8c02712
ywt <ywt>
2019-04-22 11:10:21 +0800

    20190422更新

---
+++ TestProject/Web/Controllers/Test_ConController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    /// <summary>
+    /// 控制器的测试
+    /// </summary>
+    public class Test_ConController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Name = "用户名";
+            //ViewData.Model = user;
+            return View(user);
+        }
+    }
+}
---
+++ TestProject/Web/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Age { get; set; }
+        public string Sex { get; set; }
+        public string Address { get; set; }
+    }
+}
