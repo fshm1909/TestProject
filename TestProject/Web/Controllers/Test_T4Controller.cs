8a52a83983ad5b7abc8364561427c3506f8d0464
ywt <ywt>
2019-06-05 16:25:25 +0800

    创建webapi项目

---
+++ TestProject/Web/Controllers/Test_T4Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    /// <summary>
+    /// T4模板测试
+    /// </summary>
+    public class Test_T4Controller : Controller
+    {
+        // GET: Test_T4
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
